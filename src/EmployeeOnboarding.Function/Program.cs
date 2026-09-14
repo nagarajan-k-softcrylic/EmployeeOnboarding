6fe8cc30ae43c1ebb66e5237bc28353d362f35a4
NagarajanK <nagarajan.kanagaraj@abrigo.com>
2026-09-14 19:29:58 +0530

    Add Azure Key Vault integration to Function app (matching API pattern)

--- src/EmployeeOnboarding.Function/Program.cs
+++ src/EmployeeOnboarding.Function/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +7,16 @@ using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((hostContext, configBuilder) =>
+    {
+        // ---- Azure Key Vault (Managed Identity in Azure, DefaultAzureCredential locally via az login) ----
+        var builtConfig = configBuilder.Build();
+        var keyVaultUri = builtConfig["KeyVaultUri"];
+        if (!string.IsNullOrWhiteSpace(keyVaultUri) && Uri.TryCreate(keyVaultUri, UriKind.Absolute, out var vaultUri))
+        {
+            configBuilder.AddAzureKeyVault(vaultUri, new DefaultAzureCredential());
+        }
+    })
     .ConfigureServices((hostContext, services) =>
     {
         // Application Insights telemetry is wired automatically via the
